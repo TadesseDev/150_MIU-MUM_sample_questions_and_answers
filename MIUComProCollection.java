66d0a54b59460fcc29e154c67490a7817ed16e51
tadessedev <tadessedev@gmail.com>
2024-10-26 03:29:25 +0000

    Oct 26, 2024, 6:29 AM

--- MIUComProCollection.java
+++ MIUComProCollection.java
@@ -271,9 +271,9 @@ public class MIUComProCollection {
     }
 
     int isGuthrieSequence(int[] a) {
-        if(a.length<1 || a[a.length-1] !=1)
-        return 0;
-    int num = a[0];
+        if(a.length<1 || a[a.length-1] !=1 || a[0]<1)
+            return 0;
+        int num = a[0];
         int i =0;
         while (i < a.length) {
             if (a[i] != num)
